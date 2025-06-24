429c82c9af7b09ba388e8efa985db1eb7a142fee
ilya <ilya.boltyshev@mail.ru>
2025-06-23 18:50:04 -0700

    fragment_choose_group.xml

--- app/src/main/java/com/example/myspbstu/domain/model/Group.kt
+++ app/src/main/java/com/example/myspbstu/domain/model/Group.kt
@@ -3,6 +3,7 @@ package com.example.myspbstu.domain.model
 data class Group(
     private val id : Int,
     private val name : String,
+    private val level : Int,
     private val faculty: Faculty
 ) {
 }
