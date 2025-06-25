af08a2b7ebf0e03b058c3b599f9e540ea585f0f9
ilya <ilya.boltyshev@mail.ru>
2025-06-24 18:46:28 -0700

    added safeargs and navigation between ChooseGroupFragment and ScheduleFragment

--- app/src/main/java/com/example/myspbstu/ChooseGroupFragment.kt
+++ app/src/main/java/com/example/myspbstu/ChooseGroupFragment.kt
@@ -6,7 +6,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
 import com.example.myspbstu.databinding.FragmentChooseGroupBinding
+import com.example.myspbstu.domain.model.Group
 import com.example.myspbstu.presentation.adapter.GroupsAdapter
 import com.example.myspbstu.presentation.viewmodel.ChooseGroupViewModel
 
@@ -24,6 +27,10 @@ class ChooseGroupFragment : Fragment() {
         GroupsAdapter()
     }
 
+    private val navController : NavController by lazy {
+        findNavController()
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -35,11 +42,21 @@ class ChooseGroupFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.rvGroups.adapter = groupsAdapter
+        groupsAdapter.onGroupClickListener = object : GroupsAdapter.OnGroupClickListener{
+            override fun onGroupClick(group: Group) {
+                navController.navigate(
+                    ChooseGroupFragmentDirections
+                        .actionChooseGroupFragmentToScheduleFragment(group.id)
+                )
+            }
+        }
 
-        binding.btnFindGroups.setOnClickListener {
-            val groupName = binding.editTextGroupNum.text.toString()
-            viewModel.getGroupsByName(groupName)
+        with(binding){
+            rvGroups.adapter = groupsAdapter
+            btnFindGroups.setOnClickListener {
+                val groupName = editTextGroupNum.text.toString()
+                viewModel.getGroupsByName(groupName)
+            }
         }
 
         observeLiveData()
--- app/src/main/java/com/example/myspbstu/presentation/adapter/GroupsAdapter.kt
+++ app/src/main/java/com/example/myspbstu/presentation/adapter/GroupsAdapter.kt
@@ -3,7 +3,6 @@ package com.example.myspbstu.presentation.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.EditText
 import android.widget.TextView
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
@@ -11,6 +10,8 @@ import com.example.myspbstu.R
 import com.example.myspbstu.domain.model.Group
 
 class GroupsAdapter : ListAdapter<Group, GroupsAdapter.GroupViewHolder>(GroupsDiffCallback()) {
+
+    var onGroupClickListener : OnGroupClickListener? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GroupViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(
@@ -27,11 +28,19 @@ class GroupsAdapter : ListAdapter<Group, GroupsAdapter.GroupViewHolder>(GroupsDi
         with(holder){
             tvGroupName.text = group.name
             tvFacultyName.text = group.faculty.name
+
+            itemView.setOnClickListener {
+                onGroupClickListener?.onGroupClick(group)
+            }
         }
     }
 
     class GroupViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
         val tvGroupName = itemView.findViewById<TextView>(R.id.tv_group_name)
         val tvFacultyName = itemView.findViewById<TextView>(R.id.tv_faculty_name)
+    }
+
+    interface OnGroupClickListener{
+        fun onGroupClick (group : Group)
     }
 }
