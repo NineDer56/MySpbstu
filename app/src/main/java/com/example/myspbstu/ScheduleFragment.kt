561371c687869da3f0efab3b46e7cdd6d4a3fd2c
Ilya <Ilya.boltyshev@mail.ru>
2025-07-02 17:39:19 +0300

    init new branch

--- app/src/main/java/com/example/myspbstu/ScheduleFragment.kt
+++ app/src/main/java/com/example/myspbstu/ScheduleFragment.kt
@@ -1,22 +1,17 @@
 package com.example.myspbstu
 
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.PagerSnapHelper
-import androidx.recyclerview.widget.RecyclerView
 import com.example.myspbstu.databinding.FragmentScheduleBinding
 import com.example.myspbstu.presentation.adapter.LessonsAdapter
-import com.example.myspbstu.presentation.adapter.ScheduleAdapter
 import com.example.myspbstu.presentation.viewmodel.ScheduleFragmentViewModel
-import kotlinx.coroutines.launch
 
 
 class ScheduleFragment : Fragment() {
@@ -29,10 +24,6 @@ class ScheduleFragment : Fragment() {
 
     private val lessonsAdapter by lazy {
         LessonsAdapter()
-    }
-
-    private val scheduleAdapter by lazy {
-        ScheduleAdapter()
     }
 
     private val viewModel by lazy {
@@ -58,78 +49,19 @@ class ScheduleFragment : Fragment() {
 
         binding.rvSchedule.adapter = lessonsAdapter
 
-        binding.rvWeek.layoutManager = LinearLayoutManager(
-            requireContext(),
-            LinearLayoutManager.HORIZONTAL,
-            false
-        )
-
-
-        scheduleAdapter.onDayClickListener = object : ScheduleAdapter.OnDayClickListener {
-            override fun onDayClicked(day: Int, position: Int) {
-                viewModel.updateLessons(day, position)
-            }
-        }
-
-
-        binding.rvWeek.adapter = scheduleAdapter
-        snapHelper.attachToRecyclerView(binding.rvWeek)
-
-
-        binding.rvWeek.addOnScrollListener(object : RecyclerView.OnScrollListener() {
-            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
-                super.onScrollStateChanged(recyclerView, newState)
-                if (newState != RecyclerView.SCROLL_STATE_IDLE) return
-
-                val layoutManager = recyclerView.layoutManager as? LinearLayoutManager ?: return
-                val snapView = snapHelper.findSnapView(layoutManager) ?: return
-                val position = layoutManager.getPosition(snapView)
-
-                if (position == RecyclerView.NO_POSITION) return
-
-                val offset = viewModel.getOffsetByPosition(position)
-                viewModel.setCurrentOffset(offset)
-                viewModel.updateYearAndMonthByPosition(position)
-
-                viewLifecycleOwner.lifecycleScope.launch {
-                    if (!viewModel.isWeekLoaded(offset - 1)) {
-                        viewModel.loadWeekByOffset(groupId, offset - 1)
-                    }
-                    if (!viewModel.isWeekLoaded(offset + 1)) {
-                        viewModel.loadWeekByOffset(groupId, offset + 1)
-                    }
-                }
-            }
-        })
-
-
+//        binding.rvWeek.layoutManager = LinearLayoutManager(
+//            requireContext(),
+//            LinearLayoutManager.HORIZONTAL,
+//            false
+//        )
 
 
         observeLiveData()
-
-        viewLifecycleOwner.lifecycleScope.launch {
-            viewModel.loadInitial(groupId)
-            if(!viewModel.initialScrollDone){
-                binding.rvWeek.scrollToPosition(1)
-                viewModel.initialScrollDone = true
-            }
-        }
-
     }
 
     private fun observeLiveData() {
         viewModel.lessons.observe(viewLifecycleOwner) {
             lessonsAdapter.submitList(it)
-        }
-        viewModel.schedules.observe(viewLifecycleOwner) {
-            scheduleAdapter.submitList(it)
-            Log.d("MyDebug", "observeLiveData: list updated")
-        }
-        viewModel.currentYear.observe(viewLifecycleOwner) {
-            binding.tvYear.text = it
-        }
-        viewModel.currentMonth.observe(viewLifecycleOwner) {
-            binding.tvMonth.text = it
         }
     }
 
--- app/src/main/java/com/example/myspbstu/presentation/viewmodel/ScheduleFragmentViewModel.kt
+++ app/src/main/java/com/example/myspbstu/presentation/viewmodel/ScheduleFragmentViewModel.kt
@@ -1,18 +1,12 @@
 package com.example.myspbstu.presentation.viewmodel
 
 import android.app.Application
-import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.myspbstu.data.retrofit.repository.ScheduleRepositoryImpl
 import com.example.myspbstu.domain.model.Lesson
-import com.example.myspbstu.domain.model.Schedule
 import com.example.myspbstu.domain.usecase.GetScheduleByGroupIdUseCase
-import java.time.DayOfWeek
-import java.time.LocalDate
-import java.time.format.DateTimeFormatter
-import java.util.Locale
 
 class ScheduleFragmentViewModel(
     application: Application
@@ -23,101 +17,9 @@ class ScheduleFragmentViewModel(
     private var _lessons = MutableLiveData<List<Lesson>>()
     val lessons: LiveData<List<Lesson>> get() = _lessons
 
-    private var _schedules = MutableLiveData<List<Schedule>>()
-    val schedules: LiveData<List<Schedule>> get() = _schedules
-
-    private var _currentMonth = MutableLiveData<String>()
-    val currentMonth : LiveData<String>
-        get() = _currentMonth
-
-    private var _currentYear = MutableLiveData<String>()
-    val currentYear : LiveData<String>
-        get() = _currentYear
-
-    private val scheduleList = mutableListOf<ScheduleWithOffset>()
-    private val scheduleSet = mutableSetOf<Int>() // offset values
-    private var currentWeekOffset = 0
 
     private val repository = ScheduleRepositoryImpl()
     private val getScheduleByGroupIdUseCase = GetScheduleByGroupIdUseCase(repository)
 
-    suspend fun loadInitial(groupId: Int) {
-        val offsets = listOf(-1, 0, 1)
-        offsets.forEach { offset ->
-            if (scheduleSet.add(offset)) {
-                val schedule = loadScheduleByOffset(groupId, offset)
-                scheduleList.add(ScheduleWithOffset(offset, schedule))
-            }
-        }
-        updateYearAndMonthByPosition(0)
-        scheduleList.sortBy { it.offset }
-        _schedules.value = scheduleList.map { it.schedule }
-    }
 
-    private suspend fun loadScheduleByOffset(groupId: Int, offset: Int): Schedule {
-        val monday = LocalDate.now()
-            .with(DayOfWeek.MONDAY)
-            .plusWeeks(offset.toLong())
-
-        return getScheduleByGroupIdUseCase(groupId, monday.toString())
-    }
-
-    fun isWeekLoaded(offset: Int): Boolean {
-        return scheduleSet.contains(offset)
-    }
-
-    suspend fun loadWeekByOffset(groupId: Int, offset: Int) {
-        if (scheduleSet.add(offset)) {
-            val schedule = loadScheduleByOffset(groupId, offset)
-            if (offset < scheduleList.firstOrNull()?.offset ?: 0) {
-                scheduleList.add(0, ScheduleWithOffset(offset, schedule))
-            } else {
-                scheduleList.add(ScheduleWithOffset(offset, schedule))
-            }
-            _schedules.postValue(scheduleList.sortedBy { it.offset }.map { it.schedule })
-        }
-    }
-
-    fun getOffsetByPosition(position: Int): Int {
-        return scheduleList.getOrNull(position)?.offset ?: 0
-    }
-
-    fun updateYearAndMonthByPosition(position: Int){
-        val date = scheduleList[position].schedule.week.dateStart
-        _currentYear.value = getFormattedYear(date)
-        _currentMonth.value = getFormattedMonth(date)
-    }
-
-    fun updateLessons(day: Int, schedulePosition: Int) {
-        Log.d("MyDebug", "updateLessons day $day schedulePosition $schedulePosition")
-        val schedule = scheduleList.getOrNull(schedulePosition)?.schedule ?: return
-        val lessonsForDay = schedule.days.find { it.weekday == day } ?.lessons ?: emptyList()
-        _lessons.value = lessonsForDay
-    }
-
-
-    fun setCurrentOffset(offset: Int) {
-        currentWeekOffset = offset
-    }
-
-    private fun getFormattedMonth(date: String) : String{
-        val inputFormatter = DateTimeFormatter.ofPattern("yyyy.MM.dd")
-        val outputFormatter = DateTimeFormatter.ofPattern("LLLL", Locale("ru"))
-
-        val parsedDate = LocalDate.parse(date, inputFormatter)
-        return parsedDate.format(outputFormatter).replaceFirstChar { it.titlecaseChar() }
-    }
-
-    private fun getFormattedYear(date: String) : String{
-        val inputFormatter = DateTimeFormatter.ofPattern("yyyy.MM.dd")
-        val outputFormatter = DateTimeFormatter.ofPattern("yyyy", Locale("ru"))
-
-        val parsedDate = LocalDate.parse(date, inputFormatter)
-        return parsedDate.format(outputFormatter)
-    }
-
-    data class ScheduleWithOffset(
-        val offset: Int,
-        val schedule: Schedule
-    )
 }
